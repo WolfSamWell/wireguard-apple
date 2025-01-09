1fab7e4717106bd6c1f2d707982df1e68b5a4411
PWG <pan.weigang@sand.com.cn>
2025-01-09 15:49:31 +0800

    提交代码,修改Could not build Objective-C module 'WireGuardKitC'

--- Sources/WireGuardKitC/WireGuardKitC.h
+++ Sources/WireGuardKitC/WireGuardKitC.h
@@ -4,7 +4,7 @@
 #include <Foundation/Foundation.h>
 #include "key.h"
 #include "x25519.h"
-
+#include "sys/types.h"
 /* From <sys/kern_control.h> */
 #define CTLIOCGINFO 0xc0644e03UL
 struct ctl_info {
